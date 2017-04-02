56a47abdbd82e9ebeef982e04d82fd2eb2247709
Zura Isakadze <z.isakadze@gmail.com>
2017-04-02 21:28:39 +0300

    Remove univerese dependency

--- envs.py
+++ envs.py
@@ -1,21 +1,13 @@
 import gym
 import numpy as np
-import universe
 from gym.spaces.box import Box
-from universe import vectorized
-from universe.wrappers import Unvectorize, Vectorize
-
 import cv2
-
 
 # Taken from https://github.com/openai/universe-starter-agent
 def create_atari_env(env_id):
     env = gym.make(env_id)
-    if len(env.observation_space.shape) > 1:
-        env = Vectorize(env)
-        env = AtariRescale42x42(env)
-        env = NormalizedEnv(env)
-        env = Unvectorize(env)
+    env = MyAtariRescale42x42(env)
+    env = MyNormalizedEnv(env)
     return env
 
 
@@ -29,38 +21,35 @@ def _process_frame42(frame):
     frame = frame.mean(2)
     frame = frame.astype(np.float32)
     frame *= (1.0 / 255.0)
-    frame = np.reshape(frame, [1, 42, 42])
+    #frame = np.reshape(frame, [1, 42, 42])
     return frame
 
-
-class AtariRescale42x42(vectorized.ObservationWrapper):
+class MyAtariRescale42x42(gym.ObservationWrapper):
 
     def __init__(self, env=None):
-        super(AtariRescale42x42, self).__init__(env)
+        super(MyAtariRescale42x42, self).__init__(env)
         self.observation_space = Box(0.0, 1.0, [1, 42, 42])
 
-    def _observation(self, observation_n):
-        return [_process_frame42(observation) for observation in observation_n]
+    def _observation(self, observation):
+        return _process_frame42(observation) 
 
-
-class NormalizedEnv(vectorized.ObservationWrapper):
+class MyNormalizedEnv(gym.ObservationWrapper):
 
     def __init__(self, env=None):
-        super(NormalizedEnv, self).__init__(env)
+        super(MyNormalizedEnv, self).__init__(env)
         self.state_mean = 0
         self.state_std = 0
         self.alpha = 0.9999
         self.num_steps = 0
 
-    def _observation(self, observation_n):
-        for observation in observation_n:
-            self.num_steps += 1
-            self.state_mean = self.state_mean * self.alpha + \
-                observation.mean() * (1 - self.alpha)
-            self.state_std = self.state_std * self.alpha + \
-                observation.std() * (1 - self.alpha)
+    def _observation(self, observation):
+        self.num_steps += 1
+        self.state_mean = self.state_mean * self.alpha + \
+            observation.mean() * (1 - self.alpha)
+        self.state_std = self.state_std * self.alpha + \
+            observation.std() * (1 - self.alpha)
 
         unbiased_mean = self.state_mean / (1 - pow(self.alpha, self.num_steps))
         unbiased_std = self.state_std / (1 - pow(self.alpha, self.num_steps))
-
-        return [(observation - unbiased_mean) / (unbiased_std + 1e-8) for observation in observation_n]
+        ret = (observation - unbiased_mean) / (unbiased_std + 1e-8)
+        return np.expand_dims(ret, axis=0)
