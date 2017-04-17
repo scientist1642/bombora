74c0cd6eba63bfe7ae49fd160ed3055eba62ebe6
Zura Isakadze <z.isakadze@gmail.com>
2017-04-18 00:28:47 +0300

    Reading latest model from db

--- load_model.py
+++ load_model.py
@@ -1,3 +1,5 @@
+# LOAD AND STORE DESIRED MODEL CHECKPOINT AS A FILE 
+# TODO refactor
 import sys
 import io
 import tempfile
@@ -13,8 +15,12 @@ else:
 
 db = DBReader(path)
 
+count = 0
 for idd, evtname, data, timestamp in db:
     if data['evtname'] == 'ModelCheckpoint':
+        count += 1
+        latest_params = data['state_dict']
+        # Just a test
         if data['algo'] == 'a3c' and data['arch'] == 'lstm_universe':
             from models.lstm_universe import Net
             model = Net(data['num_channels'], data['num_actions'])
@@ -29,4 +35,15 @@ for idd, evtname, data, timestamp in db:
                 value, logit, (hx, cx) = model((Variable(state), (hx, cx)))
                 print (logit)
                 print ("Everything cool!")
+
+print ('encountered {} model params'.format(count))
+
+if count > 0:
+    if len(sys.argv) == 2:
+        print ('to write, please provide file name as a second argument')
+    else:
+        outname = sys.argv[2]
+        with open(outname, 'wb') as f:
+            f.write(latest_params)
+            print ('Wrote params to {}'.format(outname))
 
